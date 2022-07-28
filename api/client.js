b8a8ea831429f3e002645a1ae4bc1f1bea8c0b0a
home <thanhnguyenvanpro123@gmail.com>
2022-07-29 01:08:59 +0700

    Infiniti loading

--- api/client.js
+++ api/client.js
@@ -1,5 +1,9 @@
 export default ($axios) => ({
-    getAllImages() {
-        return $axios.get('/images')
+    getAllImages(page, perPage) {
+        return $axios.get(`/images/?per_page=${perPage}&page=${page}`)
+    },
+
+    getAllCategories() {
+        return $axios.get(`/categories`)
     }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,74 +1,97 @@
 <template>
   <div class="container-content">
-        <div class="col" v-for="(col, index) in images"
-                        :key="index">
-            <div class="item" v-for="(img, i) in col" :key="i">
-                <img :src="img.url" 
-                    alt="">
-            </div>
-           
-        </div>
-       
+    <div class="col" v-for="(col, index) in images" :key="index">
+      <div class="item" v-for="(img, i) in col" :key="i" :name="i">
+        <img v-lazy="img.url ? img.url : baseUrl + '/' + img.image" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Vue from "vue";
+import VueLazyload from "vue-lazyload";
+
+Vue.use(VueLazyload);
+
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  attempt: 1,
+});
 export default {
-  name: 'Index',
-  layout: 'Client',
-   data() {
+  name: "Index",
+  layout: "Client",
+  data() {
     return {
-        images : [
-            
-        ]
-    }
+      busy: false,
+      images: [],
+      arrayImages: [],
+      baseUrl: process.env.API_BASE,
+      page: 1,
+      perPage: 6,
+      categories: {},
+    };
   },
-   mounted() {
-    this.getAllImages()
-    
-    window.addEventListener('resize', this.onResize(this.images))
+  mounted() {
+    const listElm = document.querySelector("body");
+    listElm.addEventListener("scroll", (e) => {
+      if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
+        this.getImageInfiniti();
+      }
+    });
+    this.getAllImages();
+    window.addEventListener("resize", () => {
+      this.onResize(this.arrayImages.slice());
+    });
   },
   methods: {
+    getImageInfiniti() {
+      this.page++;
+      this.getAllImages();
+    },
+
     async getAllImages() {
       await this.$repositories.client
-        .getAllImages()
-        .then((response) => {
-          let imgs = response.data.data
-          this.onResize(imgs)
-          
+        .getAllImages(this.page, this.perPage)
+        .then(async (response) => {
+          this.arrayImages = this.arrayImages
+            .slice()
+            .concat(response.data.data.slice());
+          this.onResize(this.arrayImages.slice());
         })
+
         .catch((error) => {
-          Error.handleError(error)
-        })
+          console.log(error);
+        });
     },
 
     onResize(imgs) {
-if (window.innerWidth > 768) {
-            if(imgs.length % 3 > 0) {
-              this.images = this.chunkArray(imgs, imgs.length/3 + 1)
-            } else {
-              this.images = this.chunkArray(imgs, imgs.length/3)
-            }
-            
+      let length = imgs.length;
+      if (window.innerWidth > 768) {
+        if (length % 3 > 0) {
+          this.images = this.chunkArray(imgs.slice(), length / 3 + 1);
+        } else {
+          this.images = this.chunkArray(imgs.slice(), length / 3);
+        }
+      } else if (window.innerWidth < 768 && window.innerWidth > 480) {
+        if (length % 2 > 0) {
+          this.images = this.chunkArray(imgs.slice(), length / 2 + 1);
+        } else {
+          this.images = this.chunkArray(imgs.slice(), length / 2);
+        }
       } else {
-       if(imgs.length % 2 > 0) {
-              this.images = this.chunkArray(imgs, imgs.length/2 + 1)
-            } else {
-              this.images = this.chunkArray(imgs, imgs.length/2)
-            }
-            
+        this.images = this.chunkArray(imgs.slice(), length);
       }
     },
 
-    chunkArray(myArray, chunk_size){
-        var results = [];
-
-        while (myArray.length) {
-            results.push(myArray.splice(0, chunk_size));
-        }
-
-        return results;
-    }
+    chunkArray(myArray, chunk_size) {
+      var results = [];
+      var array = myArray.slice();
+      while (array.length) {
+        results.push(array.splice(0, chunk_size));
+      }
+      return results;
+    },
   },
-}
+};
 </script>
